8161c617f9ba4d34a11cccd1866001454ebbfafd
kulalsk2 <subramanyakulal2000@gmail.com>
2020-09-02 14:32:25 +0530

    Updates

---
+++ precache-manifest.bd3606a4cdba5c55857e4ee5fb0862bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0042433a4d8387f5ca9c2717e44131",
+    "url": "/React-ToDoApp/index.html"
+  },
+  {
+    "revision": "84e62e99a30a775ad84d",
+    "url": "/React-ToDoApp/static/css/main.5406626d.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/React-ToDoApp/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoApp/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e62e99a30a775ad84d",
+    "url": "/React-ToDoApp/static/js/main.c2bcea3a.chunk.js"
+  },
+  {
+    "revision": "0087463942ddb24c70c2",
+    "url": "/React-ToDoApp/static/js/runtime-main.27b6d29e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-ToDoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
